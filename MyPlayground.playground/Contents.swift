d9272fc35c28c9f0677241bad3bcdfc98d0e3693
Carlos <cgomespimentel7@gmail.com>
2024-10-13 20:17:20 -0300

    initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,106 @@
+import UIKit
+/*
+
+var greeting = "Hello, playground"
+
+let int = 12
+let double = 12.34
+
+print(int)
+print(double)
+
+let name:String = "Carlos de la Torres"
+let integer:Int = 12
+let money:Double = 12.34
+let isDriver: Bool = false
+ 
+ let name:String = "carlos"
+ let years:Int = 10
+ let yearsS:String = "10"
+
+
+ let convertString = String(years)
+ let convertInt = Int(name)
+ let convertStrinS = Int(yearsS)
+
+ print(convertString)
+ print(convertInt)
+ 
+ let x:Int = 10
+ let y:Int = 20
+
+ let res = x>y
+
+ let nome1:String = "Carlos"
+ let nome2:String = "Sabrina"
+
+ let resNome = nome1 == nome2
+ 
+ let role: String = "ADMIN"
+ let userIsAuthenticate: Bool = true
+
+ if(role == "ADMIN" && userIsAuthenticate){
+     let userIsAdminAndAuthenticate: Bool = true
+     print("User is admin and authenticate")
+ }else {
+     print("User is not admin or authenticate")
+ }
+
+ var isUser: Bool = false
+ var isAdmin: Bool = false
+ let userRole:String = "ADMIN"
+
+ if(userRole == "ADMIN"){
+     isAdmin = true
+     print("User is admin")
+     
+ } else if (userRole == "USER") {
+     isUser = true
+     print("User is user")
+
+ }
+
+ let banco:String = "Bradesco"
+ switch banco {
+ case "Bradesco":
+     print("Banco Bradesco")
+ case "Itau":
+     print("Banco Itau")
+ case "Santander":
+     print("Banco Santander")
+ default:
+     print("Banco não encontrado")
+ }
+
+
+ let role: String = "admin"
+ let userIsAuthenticate: Bool = true
+
+ // Usando operador ternário
+ let userIsAdminAndAuthenticate = (role == "admin" && userIsAuthenticate) ? true : false
+ print(userIsAdminAndAuthenticate ? "User is admin and authenticate" : "User is not admin or authenticate")
+
+ var latitude:Double = 23.32
+ var longitude:Double = 23.32
+
+ let coords = (lat: longitude, long: latitude)
+ coords.lat
+ coords.long
+
+ var right:Double = 120.0
+ var up:Double = 410.0
+ var forward:Double = 110.0
+
+ let camera = (right: right, up: up, forward: forward)
+ let(x,y,z) = camera
+ print(x,y,z)
+ print(x)
+ print(y)
+ print(z)
+
+ 
+ https://docs.swift.org/swift-book/documentation/the-swift-programming-language/guidedtour/
+ https://www.youtube.com/watch?v=TBi8gyNRVTU
+ */
+
+
